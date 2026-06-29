1c2d6fe284d9d72e302f0270190e75fffe0347fa
JP <jp.seini@intentarchitect.com>
2026-06-29 14:10:48 +0200

    PROMOTED MODULES TO FINAL RELEASE

--- Tests/CSharpImportTests/ImportTest/ImportTest.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Tests/CSharpImportTests/ImportTest/ImportTest.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -35,6 +35,8 @@ namespace ImportTest.Infrastructure.Persistence
             return base.SaveChanges(acceptAllChangesOnSuccess);
         }
 
+        public bool HasDbTransaction() => Database.CurrentTransaction != null;
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
